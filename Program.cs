eb052fc6338df9e59d2903945fe36e127f8a6461
gugu <gustavocorrea2032@gmail.com>
2019-06-18 10:09:29 -0300

    Criada variável; Armazenado o valor digitado pelo usuário na variável 'numero'

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace VerificaPar
     {
         static void Main(string[] args)
         {
-           
+           //Declaração da variável do tipo inteiro
+           int numero;
+
+           Console.WriteLine ("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
         }
     }
 }
