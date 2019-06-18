917aedaf5a16e535500a36308f86225d1517ca47
gugu <gustavocorrea2032@gmail.com>
2019-06-18 10:23:02 -0300

    Colocado desvio de fluxo 'if' que irá dizer se o resto é 0

--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ namespace VerificaPar
 
            Console.WriteLine ("Digite um número: ");
             numero = int.Parse(Console.ReadLine());
+
+        if (numero % 2 == 0)
+            Console.WriteLine ("O número digitado é par!");
+        
         }
     }
 }
